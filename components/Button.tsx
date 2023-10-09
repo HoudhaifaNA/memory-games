54e64d37cfb68ecfdd8c5b3742f3fd6f2dfae29c
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-10-09 11:00:55 +0100

    Added button component

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+
+interface ButtonProps {
+  variant: 'primary' | 'secondary';
+  size: 'fit' | 'contain';
+  children: string;
+  onClick: () => void;
+}
+
+export default function Button({ variant, size, children, onClick }: ButtonProps) {
+  return (
+    <button
+      className={clsx(
+        'cursor-pointer rounded border p-1.5 text-center text-xs  font-bold capitalize text-white transition-all active:scale-95 md:p-2.5 md:text-sm',
+        variant === 'secondary' ? 'bg-amber-600 hover:bg-amber-700' : 'bg-indigo-700 hover:bg-indigo-800',
+        size === 'contain' ? 'w-full' : ''
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
