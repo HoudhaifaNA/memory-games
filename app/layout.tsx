a1cc0ba17c6fd8937f4b13e0bb7b1da47610e6fc
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-10-09 11:06:47 +0100

    Added start page

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,10 @@ const play = Play({ weight: ['400', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Memory games',
-  description: 'A great game for memory trainning',
+  description: 'Great games for memory trainning',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body className={play.className}>{children}</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import GameSetup from '@/components/GameSetup';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className='h-screen w-screen bg-indigo-950 p-8'>
+      <h1 className='text-center text-2xl font-bold uppercase text-white md:text-5xl'>Memory games</h1>
+      <GameSetup />
+    </main>
+  );
 }
