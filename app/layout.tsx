7d00ef09b5839c555ad85c7ac376d415b436fe80
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-10-10 15:12:52 +0100

    Added styling for overflow x

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={play.className}>{children}</body>
+      <body className={`${play.className} overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
